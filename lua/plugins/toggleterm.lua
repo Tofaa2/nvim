e0ac1004ae104025df87a4baeba8f3c6071d245f
Tofaa <tofik.adigozalov@gmail.com>
2025-02-10 22:41:10 +0400

    Toggle terminal

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,14 @@
+return {
+
+    {
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    config = true,
+    opts = {
+      direction = "float",
+      open_mapping = [[<c-\>]]
+    }
+  },
+
+    'da-moon/telescope-toggleterm.nvim'
+}
