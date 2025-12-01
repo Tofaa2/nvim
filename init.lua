44570274d64f8d8ce513242db68a89e4268ded02
Tofaa <tofik.adigozalov@gmail.com>
2025-12-01 10:09:28 +0400

    Amogus commit

--- init.lua
+++ init.lua
@@ -14,8 +14,12 @@ vim.opt.softtabstop = 4
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+vim.opt.sessionoptions = 'curdir,folds,globals,help,tabpages,terminal,winsize'
+
 -- PLUGINS (USES NIGHTLY FOR NATIVE PACKAGE MANAGER) --
 vim.pack.add({
+    { src = "https://github.com/nvim-tree/nvim-web-devicons"},
+    { src = "https://github.com/nanozuki/tabby.nvim"},
     { src = "https://github.com/vague2k/vague.nvim" },
     { src = "https://github.com/nvim-mini/mini.pairs" },
     { src = "https://github.com/neovim/nvim-lspconfig" },
@@ -30,12 +34,80 @@ vim.pack.add({
     { src = "https://github.com/nvim-lua/plenary.nvim"},
     { src = "https://github.com/folke/todo-comments.nvim"},
     { src = "https://github.com/nvim-telescope/telescope.nvim"},
+    { src = "https://github.com/catppuccin/nvim" },
+    { src = "https://github.com/nvim-tree/nvim-tree.lua"},
+})
+require("nvim-tree").setup({
+        -- NvimTree configuration
+    sort = {
+        sorter = "case_sensitive",
+    },
+    view = {
+        width = 30,
+    },
+    renderer = {
+        group_empty = true,
+    },
+    filters = {
+        dotfiles = true,
+    },
 })
 
 require("mini.pairs").setup()
 require("mason").setup()
 require("telescope").setup()
 require("todo-comments").setup()
+
+local theme = {
+  fill = 'TabLineFill',
+  -- Also you can do this: fill = { fg='#f2e9de', bg='#907aa9', style='italic' }
+  head = 'TabLine',
+  current_tab = 'TabLineSel',
+  tab = 'TabLine',
+  win = 'TabLine',
+  tail = 'TabLine',
+}
+require('tabby').setup({
+  line = function(line)
+    return {
+      {
+        { '  ', hl = theme.head },
+        line.sep('', theme.head, theme.fill),
+      },
+      line.tabs().foreach(function(tab)
+        local hl = tab.is_current() and theme.current_tab or theme.tab
+        return {
+          line.sep('', hl, theme.fill),
+          tab.is_current() and '' or '󰆣',
+          tab.number(),
+          tab.name(),
+          tab.close_btn(''),
+          line.sep('', hl, theme.fill),
+          hl = hl,
+          margin = ' ',
+        }
+      end),
+      line.spacer(),
+      line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
+        return {
+          line.sep('', theme.win, theme.fill),
+          win.is_current() and '' or '',
+          win.buf_name(),
+          line.sep('', theme.win, theme.fill),
+          hl = theme.win,
+          margin = ' ',
+        }
+      end),
+      {
+        line.sep('', theme.tail, theme.fill),
+        { '  ', hl = theme.tail },
+      },
+      hl = theme.fill,
+    }
+  end,
+  -- option = {}, -- setup modules' option,
+})
+
 require 'nvim-treesitter.configs'.setup {
     ensure_installed = { "c", "zig", "odin", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
     sync_install = false,
@@ -85,11 +157,11 @@ vim.cmd("set completeopt+=noselect")
 
 
 -- COLORSCHEME --
-
-vim.cmd("colorscheme rose-pine-moon")
+vim.cmd.colorscheme "catppuccin"
 vim.cmd(":hi statusline guibg=NONE")
 
 -- KEYMAPS --
+vim.api.nvim_set_keymap("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true})
 
 vim.keymap.set({ "n", "v", "x" }, "<leader>y", '"+y<CR>')
 vim.keymap.set({ "n", "v", "x" }, "<leader>p", '"+p<CR>')
@@ -113,6 +185,19 @@ vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help ta
 vim.keymap.set("n", "<leader>tih", function()
   vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = 0 }))
 end, { desc = "Toggle LSP Inlay Hints" })
+
+
+vim.api.nvim_set_keymap("n", "<leader>ta", ":$tabnew<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>tc", ":tabclose<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>to", ":tabonly<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>tn", ":tabn<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>tp", ":tabp<CR>", { noremap = true })
+-- move current tab to previous position
+vim.api.nvim_set_keymap("n", "<leader>tmp", ":-tabmove<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader><Left>", ":-tabmove<CR>", { noremap = true})
+-- move current tab to next position
+vim.api.nvim_set_keymap("n", "<leader>tmn", ":+tabmove<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader><Right>", ":+tabmove<CR>", { noremap = true })
 
 -- CUSTOM COMMANDS --
 
